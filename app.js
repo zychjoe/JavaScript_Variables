f820c60e89f6b9b82a531a0b03a8e5fe33a08857
Joe Zedd <zychjoe@gmail.com>
2022-02-26 13:03:02 -0500

    Did the exercise

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Joe"
+let lastName = "Zych"
+var age = 25 //That's the age I *feel*! (◕‿◕✿)
 
 // Exercise 2
+let fullName = `${firstName} ${lastName}`
+console.log(fullName)
 
 // Exercise 3
+let introName = `Hi! My name is ${fullName}.`
+let introCity = `I live in Brooklyn, NY!`
+let introPasstime = `I sure do love codin'!`
+
+
+let myStory = `${introName} ${introCity} ${introPasstime}`
+
+console.log(myStory)
